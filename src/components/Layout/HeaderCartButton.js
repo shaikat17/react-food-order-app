cd4de1471a2c559e1aaf88258b43c34112a8cdc9
shaikat17 <shaikatpal56@gmail.com>
2022-07-14 00:03:51 +0600

    Trying to Implement Context Api

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import classes from './HeaderCartButton.module.css'
 
 import CartIcon from '../Cart/CartIcon'
+import CartContext from '../../store/cart-context'
 
 const HeaderCartButton = (props) => {
+
+  const cartCtx = useContext(CartContext)
+
+  const numberOfItems = cartCtx.items.reduce((curNum, item) => {
+    return curNum + item.amount
+  }, 0)
   return (
     <button className={classes.button} onClick={props.onClick} >
         <span className={classes.icon}>
